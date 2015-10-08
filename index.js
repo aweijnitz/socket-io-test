efaf29d6833396cd9871dc8b328427f91f5fc5fe
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-10-08 22:33:00 +0200

    Fixed up old code to work with new socket.io

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/webroot/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
     var id = setInterval(function () {
-	socket.emit('time', 'Time is ' + new Date());
-    }, 1000);
+        socket.emit('time', 'Time is ' + new Date());
+    }, 5000);
     console.log('a user connected');
-    socket.on('disconnect', function(){
-	console.log('user disconnected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
     });
-    socket.on('chat message', function(msg){
-	socket.emit('msg', msg);
-	console.log('message: ' + msg);
+    socket.on('chat message', function (msg) {
+        io.emit('msg', msg); // Broadcast to all clients
+        console.log('message: ' + msg);
     });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
